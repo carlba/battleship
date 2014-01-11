7c35ff209253d911b222741eb20054877198052a
Carl Bäckström <genzorg@gmail.com>
2014-01-11 19:23:27 +0100

    It is possible to have more ships.
    -Refactored to more functions.

--- battleship.py
+++ battleship.py
@@ -1,4 +1,8 @@
+# -*- coding: utf-8 -*-
+import sys
 from random import randint
+
+amount_of_ships =2
 
 board = []
 
@@ -9,48 +13,97 @@ def print_board(board):
     for row in board:
         print " ".join(row)
 
-print "Let's play Battleship!"
-print_board(board)
-
 def random_row(board):
     return randint(0, len(board) - 1)
 
 def random_col(board):
     return randint(0, len(board[0]) - 1)
 
+def create_ship():
+    ship = [random_row(board),random_col(board)]
+    return ship
+
+def create_ship_list(amount):
+    ships = []
+    for ship_number in range(amount):
+        ships.append(create_ship())
+    return ships
+
+def validate_ships(ship_list,guess_row,guess_col):
+    hit= None
+    for ship in ship_list:
+        if guess_row == ship[0] and guess_col == ship[1]:
+            hit = ship
+    return hit
+
+def get_hits_on_board(board):
+    hits=[]
+
+    for i, row in enumerate(board):
+        for j,col in enumerate(row):
+            if col == "✔":
+                hits.append([i,j])
+    return hits
+
+
+def validate_guess(guess_row, guess_col):
+    return (guess_row < 0 or guess_row > 4) or (guess_col < 0 or guess_col > 4)
+
+
+def mark_position_on_board(board,row,col,char):
+    board[row][col] = char
+
+cheat = True
+
+ship_list = create_ship_list(amount_of_ships)
+
+
+
 ship_row = random_row(board)
 ship_col = random_col(board)
-print ship_row
-print ship_col
+
+if cheat:
+    print "The ships are at: %s" % repr(ship_list)
 
 # Everything from here on should go in your for loop!
 # Be sure to indent four spaces!
 
-range_list=range(4)
+range_list=range(1000)
+
+print "Let's play Battleship!"
+print_board(board)
 
 for turn in range_list:
-    print type (turn)
-    print range_list
-    
-    guess_row = int(raw_input("Guess Row:"))
-    guess_col = int(raw_input("Guess Col:"))
-    
-    if guess_row == ship_row and guess_col == ship_col:
-        print "Congratulations! You sunk my battleship!"
-        break
+
+    guess_row = int(raw_input("Guess Row:")) -1
+    guess_col = int(raw_input("Guess Col:")) -1
+
+    ship = validate_ships(ship_list,guess_row,guess_col)
+
+
+    if ship:
+        board[guess_row][guess_col] = "✔"
+
+        if len(get_hits_on_board(board)) == amount_of_ships:
+            print "You won! All my ships are gone!"
+            break
+        else:
+            print "Congratulations! You sunk one of my battleship!"
     else:
-        if (guess_row < 0 or guess_row > 4) or (guess_col < 0 or guess_col > 4):
+        if validate_guess(guess_row, guess_col):
             print "Oops, that's not even in the ocean."
         elif(board[guess_row][guess_col] == "X"):
             print "You guessed that one already."
             print range_list[:1]
             range_list.append(range_list[-1]+1)
         elif(board[guess_row][guess_col] == ""):
-            print "You entered an empty guess"            
+            print "You entered an empty guess"
         else:
             print "You missed my battleship!"
             board[guess_row][guess_col] = "X"
             if turn == 3:
                 print "Game Over"
         print turn+1
-        print_board(board)
+    print_board(board)
+
+
